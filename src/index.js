c83368bbcef036c6b7f4aaa5d83f7cb9637376e4
Aaron <acm19@outlook.com>
2022-10-23 23:07:11 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import cors from "cors";
+import { CategoryRouter,ProductRouter } from "./components";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/category", CategoryRouter);
+app.use("/api/products", ProductRouter);
+
+app.use(cors());
+
+const PORT = 3000;
+
+app.listen(PORT, () => console.log(`Server is running ${PORT}`));
